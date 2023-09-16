1fdbc98aa295cdab4cf7ffe1675214ccee27d327
Piyush Upadhyay <piyushupadhyay2566@gmail.com>
2023-09-16 09:52:29 -0400

    feat: dockerize frontend boilerplate

--- app.py
+++ app.py
@@ -13,4 +13,4 @@ def home():
 if __name__ == '__main__':
     load_dotenv()
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port, debug=true)
+    app.run(host='0.0.0.0', port=port)
